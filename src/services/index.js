965e9ad8e208592dc066b04a799084efb505f091
Goksu Okar <goksuokar@gmail.com>
2022-09-18 20:42:58 -1000

    created middlewear and moved building functions there

---
+++ src/services/index.js
@@ -0,0 +1,29 @@
+const models = require('../models');
+
+module.exports = {
+  // function that builds the product/:id/styles response
+  buildProductDetailResponse: async function(pId) {
+    const result = {"product_id": pId};
+    const styles = [];
+
+    // get the styles for related product
+    // get the skus and photos for each style id
+    const stylesInfo = await models.styles.getStylesByProductId(pId);
+    for (let i = 0; i < stylesInfo.rows.length; i ++) {
+      const style = stylesInfo.rows[i];
+      // make the queries to get the related photos and skus
+      const [resphotos, ressku] = await Promise.all([
+        models.styles.getPhotosByStyleId(style.id),
+        models.styles.getSkusByStyleId(style.id),
+      ]);
+      // add the skus and photos to object
+      style.photos = resphotos.rows;
+      Object.assign(style, ressku.rows[0]);
+      styles.push(style);
+    };
+    result.results = styles;
+    return result;
+  },
+
+};
+
